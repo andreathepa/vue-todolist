038a3863ff57e645e17f4ada786cdf23cf268ec1
andreathepa <andreadepa32@gmail.com>
2023-06-10 15:24:45 +0200

    add click event: add and delete task

--- js/script.js
+++ js/script.js
@@ -4,10 +4,40 @@ createApp({
     data() {
       return{
         
-        
+        title_app: 'Vue Toobolist',
+        tasks: [
+          {
+            text: 'Seguire lezione',
+            done:false
+          },
+          {
+            text: 'Pulire a terra',
+            done:false
+          },
+          {
+            text: 'Finire Batman',
+            done:false
+          },
+          {
+            text: 'Comprare cornetti',
+            done:false
+          },
+          {
+            text: 'Leggere libro',
+            done:true
+          },
+        ]
 
 
 
+      }
+    },
+    methods: {
+      changeTaskStatus(index){
+        this.tasks[index].done = !this.tasks[index].done
+      },
+      deleteTask(index){
+        this.tasks.splice(index,1);
       }
     },
     
--- readme.md
+++ readme.md
@@ -5,3 +5,12 @@ Rifare l'esercizio della to do list visto a lezione. Create un array di oggetti 
 BONUS
 
 Aggiungete la funzionalità di edit della task
+
+Svolgimento
+
+1 Creare struttura statica
+
+2 Inserire v for per ciclare li
+
+3 Inserire una condizione per la quale se è vera viene attivata una classe
+
