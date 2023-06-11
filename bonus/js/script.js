097dbfac9eaa7ddd6d4094649c89067f180eaa3e
andreathepa <andreadepa32@gmail.com>
2023-06-11 10:25:29 +0200

    starting bonus

---
+++ bonus/js/script.js
@@ -0,0 +1,55 @@
+const {createApp} = Vue;
+
+createApp({
+    data() {
+      return{
+        
+        title_app: 'Vue Toobolist',
+        new_task: [],
+        tasks: [
+          {
+            text: 'Seguire lezione',
+            done:false
+          },
+          {
+            text: 'Pulire a terra',
+            done:false
+          },
+          {
+            text: 'Finire Batman',
+            done:false
+          },
+          {
+            text: 'Comprare cornetti',
+            done:false
+          },
+          {
+            text: 'Leggere libro',
+            done: false
+          },
+        ]
+
+
+
+      }
+    },
+    methods: {
+      changeTaskStatus(index){
+        this.tasks[index].done = !this.tasks[index].done
+      },
+      deleteTask(index){
+        this.tasks.splice(index,1);
+      },
+      addTask(){
+        let obj = {
+          text: this.new_task,
+          done: false
+        }
+
+        this.tasks.push(obj);
+
+        this.new_task = '';
+      }
+    },
+    
+  }).mount('#app');
